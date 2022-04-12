7951ab9b6e94bae509976fac9b9efc432929dfc5
Ilya Baryshnikov <zlonast3@gmail.com>
2022-04-13 01:05:04 +0800

    add Config

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
 import Lib (startSever)
+import Config (Config (..))
+import qualified Data.ByteString as B
+import qualified Data.Yaml as Y
 
 main :: IO ()
-main = startSever
+main = do
+  config <- Y.decodeThrow =<< B.readFile "src/Config.yaml"
+  startSever config
--- src/Config.hs
+++ src/Config.hs
@@ -1,11 +1,46 @@
 module Config where
 
+import Data.Text (Text)
+import Data.Yaml (FromJSON)
+import GHC.Generics (Generic)
+import Database.PostgreSQL.Simple
+  ( Connection,
+    In (..),
+    Only (..),
+    Query (..))
 
+{-
+log levels
+-}
+data Priority
+  = -- | Debug messages
+    Debug
+  | -- | Notable information that requires no immediate action.
+    Info
+  | -- | Something is probably wrong, and we should investigate.
+    Warn
+  deriving (FromJSON, Generic, Eq, Ord, Show)
 
+{-
+config
+-}
+data Config = Config
+  { host :: Text,
+    dbname :: Text,
+    limit :: Int,
+    loglevel :: Priority,
+    tables :: Tables
+  }
+  deriving (FromJSON, Generic, Eq, Show)
 
-
-
-
-
-
-
+{-
+tables in the database
+-}
+data Tables = Tables
+  { tableUserString :: String,
+    tableCatString :: String,
+    tableImGString :: String,
+    tableNewsImGString :: String,
+    tableNewsRowString :: String
+  }
+  deriving (FromJSON, Generic, Eq, Ord, Show)
